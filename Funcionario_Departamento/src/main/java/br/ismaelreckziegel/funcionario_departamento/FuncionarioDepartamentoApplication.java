c5c5358610adf37d4cf207e3f185d034392afe8c
Ismael Reckziegel <ismaelreck@gmail.com>
2026-08-21 11:04:25 -0300

    feat: configura conexao com banco e estrutura de pacotes inicial

---
+++ Funcionario_Departamento/src/main/java/br/ismaelreckziegel/funcionario_departamento/FuncionarioDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package br.ismaelreckziegel.funcionario_departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioDepartamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionarioDepartamentoApplication.class, args);
+    }
+
+}
